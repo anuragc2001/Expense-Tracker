c28ab1e003265cdd6830d642d497e02c02e99c46
Anurag Chakraborty <chakraborty.anurag01@gmail.com>
2022-01-16 21:36:03 +0530

    Initial Component Setup

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+function ExpenseItem({ title, amount, date }) {
+
+
+    return (
+        <Card className="expense-item">
+            <div>
+                <ExpenseDate date={date} />
+            </div>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+            </div>
+            <div className="expense-item__price">
+                {amount} INR
+            </div>
+        </Card>
+    );
+}
+
+export default ExpenseItem;
